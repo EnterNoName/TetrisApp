909a451abfdd18488a3bd3b58076827d6c8174a1
MichaelRayven <themikerayven@gmail.com>
2022-07-10 23:30:48 +0700

    Updated PauseFragment

--- src/main/java/com/example/tetris/fragments/PauseFragment.java
+++ src/main/java/com/example/tetris/fragments/PauseFragment.java
@@ -1,37 +1,36 @@
-package com.example.tetris.Fragments;
+package com.example.tetris.fragments;
 
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
-import com.example.tetris.Activities.GameActivity;
-import com.example.tetris.Models.Tetris;
+import com.example.tetris.util.JSON;
+import com.example.tetris.view.Tetris;
 import com.example.tetris.R;
-import com.example.tetris.Utils.JSON;
 
 public class PauseFragment extends Fragment {
     public PauseFragment() {
-        super(R.layout.fragment_pause);
+        super(R.layout.pause_fragment);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         Tetris.GameStatistics stats = requireArguments().getParcelable("statistics");
 
-        GameActivity activity = (GameActivity) requireActivity();
         view.findViewById(R.id.resumeBtn).setOnClickListener(v -> {
-            activity.game.resumeThreads();
-            activity.getSupportFragmentManager().beginTransaction().remove(PauseFragment.this).commit();
+            requireParentFragment().getChildFragmentManager().beginTransaction().remove(PauseFragment.this).commit();
+            ((GameFragment) requireParentFragment()).onGameResume();
         });
         view.findViewById(R.id.exitActivityBtn).setOnClickListener(v -> {
+            ((GameFragment) requireParentFragment()).onStopGame();
             JSON.writeStats(getContext(), stats);
-            activity.finish();
+            requireActivity().getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.fragment_container_view, MainMenuFragment.class, null)
+                    .commit();
         });
         super.onViewCreated(view, savedInstanceState);
     }
