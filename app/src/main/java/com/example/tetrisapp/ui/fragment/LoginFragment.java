74cd0e16e1eb80334d2a6a0cd7eae6a22316231c
MichaelRayven <themikerayven@gmail.com>
2022-12-20 15:11:59 +0700

    Added LoginFragment

--- app/src/main/java/com/example/tetrisapp/ui/fragment/LoginFragment.java
+++ app/src/main/java/com/example/tetrisapp/ui/fragment/LoginFragment.java
@@ -1,24 +1,89 @@
 package com.example.tetrisapp.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.tetrisapp.R;
 import com.example.tetrisapp.databinding.FragmentLoginBinding;
+import com.example.tetrisapp.ui.activity.MainActivity;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginFragment extends Fragment {
 
+    private static final String TAG = "LoginFragment";
     private FragmentLoginBinding binding;
+
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
+
+    private FirebaseAuth mAuth;
+    private final ActivityResultLauncher<IntentSenderRequest> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(), result -> {
+        try {
+            SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(result.getData());
+            String idToken = credential.getGoogleIdToken();
+            if (idToken !=  null) {
+                AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+                mAuth.signInWithCredential(firebaseCredential)
+                        .addOnCompleteListener(requireActivity(), task -> {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithCredential:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithCredential:failure", task.getException());
+                                }
+                            }
+                        );
+                // Got an ID token from Google. Use it to authenticate
+                // with Firebase.
+                Log.d(TAG, "Got ID token.");
+            }
+        } catch (ApiException e) {
+            Log.d(TAG, e.getLocalizedMessage());
+        }
+    });
+    
+    private Mode mode = Mode.SIGNIN;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        mAuth = FirebaseAuth.getInstance();
+        oneTapClient = Identity.getSignInClient(requireActivity());
+        signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        .setFilterByAuthorizedAccounts(false)
+                        .build())
+                .setAutoSelectEnabled(true)
+                .build();
     }
 
     @Override
@@ -26,5 +91,101 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentLoginBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initClickListeners();
+    }
+
+    private void initClickListeners() {
+        binding.btnAction.setOnClickListener(v -> {
+            String email = binding.etEmail.getText().toString();
+            String password = binding.etPassword.getText().toString();
+
+            switch (mode) {
+                case SIGNIN:
+                    signIn(email, password);
+                    break;
+                case SIGNUP:
+                    createAccount(email, password);
+                    break;
+            }
+
+        });
+        binding.btnSignInGoogle.setOnClickListener(v -> {
+            oneTapClient.beginSignIn(signInRequest)
+                    .addOnCompleteListener(requireActivity(), result -> {
+                        activityResultLauncher.launch(new IntentSenderRequest.Builder(result.getResult().getPendingIntent().getIntentSender()).build());
+                    })
+                    .addOnFailureListener(requireActivity(), e -> {
+                        Log.d(TAG, e.getLocalizedMessage());
+                    });
+        });
+        binding.btnBack.setOnClickListener(v -> {
+            Navigation.findNavController(binding.getRoot()).popBackStack();
+        });
+        binding.btnSwitchAction.setOnClickListener(v -> {
+            if (mode == Mode.SIGNIN) {
+                mode = Mode.SIGNUP;
+            } else {
+                mode = Mode.SIGNIN;
+            }
+
+            updateUI();
+        });
+    }
+    
+    private void updateUI() {
+        switch (mode) {
+            case SIGNIN:
+                binding.btnAction.setText(getString(R.string.sign_in));
+                binding.btnSwitchAction.setText(getString(R.string.register_account));
+                break;
+            case SIGNUP:
+                binding.btnAction.setText(getString(R.string.sign_up));
+                binding.btnSwitchAction.setText(getString(R.string.login_account));
+                break;
+        }
+    }
+
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity(), task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(requireContext(), "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity(), task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(requireContext(), "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+    }
+    
+    private enum Mode {
+        SIGNIN,
+        SIGNUP;
     }
 }
