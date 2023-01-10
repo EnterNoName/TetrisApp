4455f69159090e2b3f5e23b2c8630ebb858b786e
MichaelRayven <themikerayven@gmail.com>
2023-01-10 19:37:38 +0700

    Added a method to simplify binding event listeners to presence channels

--- app/src/main/java/com/example/tetrisapp/util/PusherUtil.java
+++ app/src/main/java/com/example/tetrisapp/util/PusherUtil.java
@@ -2,10 +2,16 @@ package com.example.tetrisapp.util;
 
 import android.util.Log;
 
+import androidx.navigation.Navigation;
+
+import com.example.tetrisapp.R;
 import com.pusher.client.Pusher;
 import com.pusher.client.PusherOptions;
 import com.pusher.client.channel.PresenceChannel;
 import com.pusher.client.channel.PresenceChannelEventListener;
+import com.pusher.client.channel.PusherEvent;
+import com.pusher.client.channel.SubscriptionEventListener;
+import com.pusher.client.channel.User;
 import com.pusher.client.connection.ConnectionEventListener;
 import com.pusher.client.connection.ConnectionState;
 import com.pusher.client.connection.ConnectionStateChange;
@@ -14,6 +20,7 @@ import com.pusher.client.util.UrlEncodedConnectionFactory;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class PusherUtil {
     public final static String TAG = "Pusher";
@@ -54,17 +61,94 @@ public class PusherUtil {
         return pusher;
     }
 
+    public static void bindPresenceChannel(
+            PresenceChannel channel,
+            String eventName,
+            SubscriptionEventListener listener
+    ) {
+        channel.bind(eventName, new PresenceChannelEventListener() {
+            @Override
+            public void onUsersInformationReceived(String channelName, Set<User> users) {}
+
+            @Override
+            public void userSubscribed(String channelName, User user) {}
+
+            @Override
+            public void userUnsubscribed(String channelName, User user) {}
+
+            @Override
+            public void onAuthenticationFailure(String message, Exception e) {}
+
+            @Override
+            public void onSubscriptionSucceeded(String channelName) {}
+
+            @Override
+            public void onEvent(PusherEvent event) {
+                listener.onEvent(event);
+            }
+        });
+    }
+
     public static void unsubscribePresence() {
         presenceChannelMap.keySet().forEach(val -> {
             pusherInstance.unsubscribe(val);
         });
     }
 
-    public static PresenceChannel getPresenceChannel(Pusher pusherInstance, String channelName, PresenceChannelEventListener listener) {
-        if (!presenceChannelMap.containsKey(channelName)) {
-            presenceChannelMap.put(channelName, pusherInstance.subscribePresence(channelName, listener));
-        }
+    public static PresenceChannel getPresenceChannel(
+            String channelName
+    ) {
+        if (pusherInstance == null) return null;
+
+        return presenceChannelMap.getOrDefault(channelName, null);
+    }
+
+    public static PresenceChannel getPresenceChannel(
+            String channelName,
+            UserActionEvent userSubscribedListener,
+            UserActionEvent userUnsubscribedListener,
+            AuthenticationFailedEvent authenticationFailedEvent
+    ) {
+        if (getPresenceChannel(channelName) != null) return getPresenceChannel(channelName);
+
+        presenceChannelMap.put(channelName, pusherInstance.subscribePresence(channelName, new PresenceChannelEventListener() {
+            @Override
+            public void onUsersInformationReceived(String channelName, Set<User> users) {
+                for (User user : users) {
+                    userSubscribed(channelName, user);
+                }
+            }
+
+            @Override
+            public void userSubscribed(String channelName, User user) {
+                userSubscribedListener.call(channelName, user);
+            }
+
+            @Override
+            public void userUnsubscribed(String channelName, User user) {
+                userUnsubscribedListener.call(channelName, user);
+            }
+
+            @Override
+            public void onAuthenticationFailure(String message, Exception e) {
+                authenticationFailedEvent.call(message, e);
+            }
+
+            @Override
+            public void onSubscriptionSucceeded(String channelName) {}
+
+            @Override
+            public void onEvent(PusherEvent event) {}
+        }));
 
         return presenceChannelMap.get(channelName);
+    }
+
+    public interface UserActionEvent {
+        void call(final String channelName, final User user);
+    }
+
+    public interface AuthenticationFailedEvent {
+        void call(final String message, final Exception e);
     }
 }
