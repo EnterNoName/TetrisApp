13de655ac208eb78f8e5333584f7c311f709b678
MichaelRayven <themikerayven@gmail.com>
2023-01-27 13:54:57 +0700

    Removed invalidate data route

--- app/src/main/java/com/example/tetrisapp/data/remote/GameService.java
+++ app/src/main/java/com/example/tetrisapp/data/remote/GameService.java
@@ -1,7 +1,6 @@
 package com.example.tetrisapp.data.remote;
 
 import com.example.tetrisapp.model.remote.response.DefaultPayload;
-import com.example.tetrisapp.model.remote.request.InvalidatePlayerDataPayload;
 import com.example.tetrisapp.model.remote.request.TokenPayload;
 
 import retrofit2.Call;
@@ -11,9 +10,6 @@ import retrofit2.http.POST;
 public interface GameService {
     @POST("game/start")
     Call<DefaultPayload> startGame(@Body TokenPayload body);
-
-    @POST("game/player/invalidatedata")
-    Call<DefaultPayload> invalidatePlayerData(@Body InvalidatePlayerDataPayload body);
 
     @POST("game/player/lost")
     Call<DefaultPayload> declareLoss(@Body TokenPayload body);
