1cf9eeeab0737610154091db1f44f3f217643102
MichaelRayven <themikerayven@gmail.com>
2023-02-08 20:25:26 +0700

    Added limit to GetScorePayload

--- app/src/main/java/com/example/tetrisapp/model/remote/request/GetScorePayload.java
+++ app/src/main/java/com/example/tetrisapp/model/remote/request/GetScorePayload.java
@@ -5,9 +5,12 @@ import com.google.gson.annotations.SerializedName;
 public class GetScorePayload extends TokenPayload {
     @SerializedName("page")
     public int page;
+    @SerializedName("limit")
+    public int limit;
 
-    public GetScorePayload(String idToken, int page) {
+    public GetScorePayload(String idToken, int page, int limit) {
         super(idToken);
         this.page = page;
+        this.limit = limit;
     }
 }
