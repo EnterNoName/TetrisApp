d9ece406fef96da8cbbb97918c32c23cf8f9641f
MichaelRayven <themikerayven@gmail.com>
2023-02-08 20:22:27 +0700

    Update database

--- app/src/main/java/com/example/tetrisapp/data/local/db/AppDatabase.java
+++ app/src/main/java/com/example/tetrisapp/data/local/db/AppDatabase.java
@@ -1,12 +1,18 @@
 package com.example.tetrisapp.data.local.db;
 
+import androidx.room.AutoMigration;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.tetrisapp.data.local.dao.LeaderboardDao;
 import com.example.tetrisapp.model.local.entity.LeaderboardEntry;
 
-@Database(entities = {LeaderboardEntry.class}, version = 1, exportSchema = false)
+@Database(
+        entities = {LeaderboardEntry.class},
+        version = 2
+)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract LeaderboardDao leaderboardDao();
 }
